4c18a557e5d02476921f956b54f68a36b907a252
Bart <bartoszpudlo07@gmail.com>
2024-11-17 20:26:24 +0100

    added LT100.py

---
+++ binary_trees/Lt108.py
@@ -0,0 +1,32 @@
+arr = [1,2,3,4,5]
+
+n = len(arr)
+
+med = (n - 1) // 2
+root = arr[med]
+print(root)
+
+
+
+### SOLUTION
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        return root
+
+
+
+
+
