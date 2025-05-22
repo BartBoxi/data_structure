c916ee1b10476301ed39ba9dce4582f76a171484
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-05-23 00:19:28 +0200

    Testing DFS

--- trees/main.py
+++ trees/main.py
@@ -29,3 +29,81 @@ print(root_node)
 
 # Nodes 0,1,2,3,
 # Edges (0,1), (0,2), (1,2), (2,3)
+
+# adj_matrix = [
+# 	[0,1,1,0],
+# 	[1,0,1,0],
+# 	[1,1,0,1],
+# 	[0,0,1,0]
+# ]
+#
+# #check if edge (0,1) exists:
+# print(adj_matrix[0][1]) #Output: 1
+
+### ex 3 adjacency list
+
+#Nodes A,B,C,D
+#Edges: (A,B), (A,C), (B,C), (C,D)
+
+### Typical Binary Tree representation
+
+# class TreeNode:
+# 	def __init__(self, val, left, right):
+# 		self.val = val
+# 		self.left = left
+# 		self.right = right
+
+
+### DFS Depth First search
+#
+# def dfs(node):
+# 	if node == None:
+# 		return
+#
+# 	dfs(node.left)
+# 	dfs(node.right)
+# 	return
+
+## preorder traversal
+
+# def preorder_dfs(node):
+# 	if not node:
+# 		return
+# 	print(node.val)
+# 	preorder_dfs(node.left)
+# 	preorder_dfs(node.right)
+# 	return
+
+
+class TreeNode:
+	def __init__(self, val=0, children = None):
+		self.val = val
+		self.children = children if children is not None else []
+
+def preorder_dfs(node):
+	if not node:
+		return
+	print(node.val)
+
+	for child in node.children:
+		preorder_dfs(child)
+	return
+
+node0 = TreeNode(0)
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+
+# Define relationships
+node0.children = [node1, node2]
+node1.children = [node3, node4]
+node2.children = [node5]
+node3.children = [] # Leaf node
+node4.children = [node6]
+node5.children = [] # Leaf node
+node6.children = [] # Leaf node
+
+preorder_dfs(node0)
