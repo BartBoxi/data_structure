ee561599ff776eaf60d5559bcab67f902c154c4b
Bart <bartoszpudlo07@gmail.com>
2025-06-15 22:48:16 +0200

    sliding window with binary input

---
+++ arrays/sliding_binarystring.py
@@ -0,0 +1,19 @@
+def find_length(s:str) -> int:
+    left = curr = ans = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+             curr +=1
+
+        while curr > 1:
+            if s[left] == "0":
+                curr -=1
+            left +=1
+        ans = max(ans, right - left +1)
+    return ans
+
+s = "1101100111"
+
+print(find_length(s))
+
+
+
