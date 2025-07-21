5b5894692ac746fc2f5ce787631daad8e28b7073
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-07-21 23:11:13 +0200

    Updated the new task

--- interview_questions/max_occupancy.py
+++ interview_questions/max_occupancy.py
@@ -1,36 +1,47 @@
-# A entry/exit event recorded at building entrance
-class Event {
-  def __init__(self, badgeId, isEntry, timestamp):
-    self.badgeId = badgeId
-    self.isEntry = isEntry # if false, event is Exit
-    self.timestamp = timestamp # number of seconds since midnight
-}
-
-# Print max number of people in the building and timestamp when it occurs
-def findMaxOccupancy(events) {
-  # Implement me!
-}
-
-
-def count_occupancy(sorted_events, time):
- ans = 0
- occupancy = 0
-  for event in sorted_events:
-	if event.time < time:
-		if event.isEntry = True:
-			occupancy +=1
+def count_occupancy(events, time):
+	sorted_events = sorted(events, key= lambda x : x[2])
+	ans = 0
+	occupancy = 0
+	for event in sorted_events:
+		if event[2] <= time:
+			if event[1] == True:
+				occupancy +=1
+			else:
+				occupancy -=1
 		else:
-			occupancy +=1
-	else:
-		break
-   return occupancy # here we got occupancy for specific time or i would say          until specific time
+			break
+	return occupancy # here we got occupancy for specific time or i would say          until specific time
 
-def find_max_occupancy(events):
-	max_occupancy = 0
-	time_for_max_occupancy = 0
-	for sorted_event.time in events:
-		occupancy_at_time =   count_occupancy(sorted_events,sorted_event.time)
-if occupancy_at_time > max_occupancy: # here we will get max value each time it is bigger than the current max value saved
-max_occupancy = occupancy_at_time
-time_for_max_occupancy = sorted_event.time
-	return max_occupancy, time_for_max_occupancy
+events = [
+    (2, True, 3),
+    (1, True, 3),
+    (3, True, 4),
+    (1, False, 5),
+    (4, False, 6),
+    (5, True, 6),
+    (3, False, 6),
+    (2, False, 7),
+    (4, False, 7),
+    (5, False, 7),
+]
+
+time = 7
+
+print(count_occupancy(events, time))
+
+
+
+
+
+#
+#
+#
+# def find_max_occupancy(events):
+# 	max_occupancy = 0
+# 	time_for_max_occupancy = 0
+# 	for sorted_event.time in events:
+# 		occupancy_at_time =   count_occupancy(sorted_events,sorted_event.time)
+# if occupancy_at_time > max_occupancy: # here we will get max value each time it is bigger than the current max value saved
+# max_occupancy = occupancy_at_time
+# time_for_max_occupancy = sorted_event.time
+# 	return max_occupancy, time_for_max_occupancy
