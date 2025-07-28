a8c340f9a6463ab87fad12c21d17c6134efe772a
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-07-28 23:49:29 +0200

    Added new tasks for missing_num.py

--- hashing/missing_num.py
+++ hashing/missing_num.py
@@ -1,11 +1,11 @@
 def missingNumber(nums) -> int:
-    ##new_set = set()
-    new_lst = []
-    for i in range(0,len(nums)):
-        new_lst.append(i)
-    return new_lst
+    n = len(nums)
+    full_set = set(range(n +1))
+    nums_set = set(nums)
+    missing = full_set - nums_set
+    return missing.pop()
 
 
-nums = [0,2,1]
+nums = [9,6,4,2,3,5,7,0,1]
 
 print(missingNumber(nums))
