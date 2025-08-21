8d5d8e80efa15cc3bbfedbb890a420edf6b880fb
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-08-21 23:43:52 +0200

    Showing final solution with )(n * log n)

--- hashing/find_players.py
+++ hashing/find_players.py
@@ -1,37 +1,48 @@
+from typing import List
 from collections import defaultdict
-from typing import List
-
 
 def findWinners(matches : List[List[int]]) -> List[List[int]]:
-    loosers = {}
-    for list in matches:
-        for number in list:
-            if number[1] not in loosers:
-                loosers.add(number)
-        return loosers
+    zero_loss = set()
+    one_loss = set()
+    more_losses = set()
 
-
-
-
-
-
-from typing import List
-
-
-def findWinners(matches : List[List[int]]) -> List[List[int]]:
-    loosers = {}
-    winners = {}
     for match in matches:
-        loser = match[1]
         winner = match[0]
-        loosers.append(loser)
-        winners.add(winner)
+        loser = match[1]
+        if winner not in one_loss and winner not in more_losses: ### important was and here
+            zero_loss.add(winner)
+        if loser in zero_loss:
+            zero_loss.remove(loser)
+            one_loss.add(loser)
+        elif loser in one_loss:
+            one_loss.remove(loser)
+            more_losses.add(loser)
+        elif loser in more_losses:
+            more_losses = more_losses
+        else:
+            one_loss.add(loser)
+    return [sorted(list(zero_loss)), sorted(list(one_loss))]
 
-    answer = winners - loosers
-    return answer
-
-matches = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]
-
+matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
 print(findWinners(matches))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ hashing/testing.py
@@ -0,0 +1,10 @@
+matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+
+
+
+winners = set()
+for match in matches:
+    winner = match[0]
+    looser = match[1]
+    winners.add(winner)
+    print(winners)
