ef639f88be73b7a9e06110fa0bc384ba9b0f39b7
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-09-02 23:53:56 +0200

    Solved annograms.py and trying to figure out how to solve minConsecutiveCards.py

--- hashing/minConsecutiveCards.py
+++ hashing/minConsecutiveCards.py
@@ -4,10 +4,16 @@ from typing import List
 def minimumCardPickup(cards: List[int]):
     dic = defaultdict(list)
     for i in range(len(cards)):
-        dic[cards[i].append(i)] ## here we are calling the value
+        dic[cards[i]].append(i) ## here we are calling the value
                                 # at specific index
 
-        print(dic)
+    ans = float("inf")
+    for key in dic:
+        arr = dic[key]
+        for i in range(len(arr) -1):
+            ans = min(ans, arr[i + 1] - arr[i] + 1)
+
+    return ans if ans < float("inf") else -1
 
 
 cards = [1, 2, 6, 2, 1]
