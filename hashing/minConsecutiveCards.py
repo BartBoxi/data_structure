d3a84fa2669e38e717a2f95a3cda34dc830968ed
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-09-03 23:33:43 +0200

    Added some more comments there to make this solution more clear

--- hashing/minConsecutiveCards.py
+++ hashing/minConsecutiveCards.py
@@ -6,14 +6,20 @@ def minimumCardPickup(cards: List[int]):
     for i in range(len(cards)):
         dic[cards[i]].append(i) ## here we are calling the value
                                 # at specific index
+    ###after this one that's how our dic will look like
+    #dic = {1: [0, 4], 2: [1, 3], 6: [2]}
 
-    ans = float("inf")
-    for key in dic:
-        arr = dic[key]
-        for i in range(len(arr) -1):
-            ans = min(ans, arr[i + 1] - arr[i] + 1)
+    ans = float("inf")  #so we are setting up this value to infitiy just as a trick to solve it
+    for key in dic: # key = 2
+        arr = dic[key] # = dic[1,3] ==> 2
+        for i in range(len(arr) -1): # range(len(2 - 1)) ==> range(len(1))
+            ans = min(ans, arr[i + 1] - arr[i] + 1) # we calculate from end ot begining
+            ## With i = 0, this becomes: ans = min(ans, arr[1] - arr[0] + 1)
+            ## ans = min(ans, 3 - 1, + 1)
+            ## ans = min(ans, 3)
 
-    return ans if ans < float("inf") else -1
+    return ans if ans < float("inf") else -1 # here we need to statement as it give answer only
+### if the answer exist or if there is no ans then it will give us -1
 
 
 cards = [1, 2, 6, 2, 1]
