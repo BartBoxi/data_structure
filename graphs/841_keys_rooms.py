379ff23e5f1a86a202b569a7dd1b102531f4ab8b
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-09-28 22:50:25 +0200

    Added one more task from DFS for graphs.

---
+++ graphs/841_keys_rooms.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+def KeysandRooms(rooms):
+
+    visited = {0}
+    to_do = [0]
+
+    while to_do:
+        current_room = to_do.pop()
+        for key in rooms[current_room]:
+            if key not in visited:
+                visited.add(key)
+                to_do.append(key)
+    return len(visited) == len(rooms)
+
+
+rooms = [[1],[2],[3],[]]
+
+print(KeysandRooms(rooms))
