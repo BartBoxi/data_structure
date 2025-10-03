217ed082e4793a6c56cd8d77bd769a4627fb4eec
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-10-03 09:24:10 +0200

    added description to the task

--- hackerrank tasks /stringvalidator.py
+++ hackerrank tasks /stringvalidator.py
@@ -1,3 +1,9 @@
+### task is In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if  has any digits. Otherwise, print False.
+# In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+
 if __name__ == '__main__':
     s = input()
     alnum, alpha, digit, lower, upper = False, False, False, False, False
