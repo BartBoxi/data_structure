1e6f59f7d5b5d289ae56c342b280d6a4a0f7b8a5
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-10-14 22:48:32 +0200

    Added bfs solution for 1971

---
+++ graphs/find_if_path_exists.py
@@ -0,0 +1,40 @@
+import collections
+from typing import List
+
+from collections import deque, defaultdict
+
+### BFS Solution
+class Solution():
+    def findifthepathexists(self, n:int, edges:List[List[int]], source:int, destination:int):
+        graph = collections.defaultdict(list)
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        # storing all the nodes to be visited in the queue
+        seen = [False] * n
+        seen[source] = True
+        queue = collections.deque([source])
+
+        while queue:
+            current_node = queue.popleft()
+            if current_node == destination:
+                return True
+
+            for next_node in graph[current_node]:
+                if not seen[next_node]:
+                    seen[next_node] = True
+                    queue.append(next_node)
+        return False
+
+
+edges = [[0,1],[1,2],[2,0]]
+n = 3
+source = 0
+destination = 2
+
+
+solution = Solution()
+print(solution.findifthepathexists(n, edges, source, destination))
+
+
