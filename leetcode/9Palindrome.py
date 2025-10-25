b5f5d507a2a7fb502cd589cf22eb755b395f83ba
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-10-25 22:26:36 +0200

    Added solution for the palindrome number

---
+++ leetcode/9Palindrome.py
@@ -0,0 +1,16 @@
+
+
+
+def isPalindrome(x:int) -> bool:
+    if x > 0:
+        rev_num = int(str(x)[::-1])
+        if x == rev_num:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+    
+x = 121
+print(isPalindrome(x))
