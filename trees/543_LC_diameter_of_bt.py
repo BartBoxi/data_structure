5ccd53145eeb002d2b305e14d584ac403a71a2a9
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-10-27 22:53:35 +0100

    added solution for task 543. still need to think more about it as it is not very easy

---
+++ trees/543_LC_diameter_of_bt.py
@@ -0,0 +1,27 @@
+from collections import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+
+def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    diameter = 0
+    def longest_path(node):
+        if not node:
+            return 0
+        nonlocal diameter
+        left_path = longest_path(node.left)
+        right_path = longest_path(node.right)
+
+        diameter = max(diameter, left_path + right_path)
+        return max(left_path, right_path) + 1
+    longest_path(root)
+    return diameter
+
+    
