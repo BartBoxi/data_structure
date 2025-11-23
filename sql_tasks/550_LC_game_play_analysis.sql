bab0f2f14d7badcd13d13d96d1e100fb3383542e
bartoszpudlo <bartoszpudlo07@gmail.com>
2025-11-23 23:55:21 +0100

    Added also optimazied solution to speed it up a bit

--- sql_tasks/550_LC_game_play_analysis.sql
+++ sql_tasks/550_LC_game_play_analysis.sql
@@ -23,3 +23,32 @@ select
 from Activity a
 left join second_login s
 on s.player_id = a.player_id;
+
+
+-- here is even faster solution without left join at the end 
+
+# Write your MySQL query statement below
+
+with first_login as (
+    select 
+        player_id, 
+        min(event_date) as first_login
+    from Activity
+    group by player_id
+),
+
+second_login as (
+select 
+    a.player_id as player_id, 
+    a.event_date as second_login
+from Activity a
+join first_login f
+    on a.player_id = f.player_id
+where 
+    a.event_date = f.first_login + interval 1 day)
+
+select 
+   round(
+    (select count(*) from second_login)
+    / (select count(distinct player_id) from Activity),2)
+     as fraction;
