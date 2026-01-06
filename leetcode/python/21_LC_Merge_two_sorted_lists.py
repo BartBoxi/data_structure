da70dbead60864cba3ab2d44990f750378999e2d
bartoszpudlo <bartoszpudlo07@gmail.com>
2026-01-06 01:02:49 +0100

    Added solution that leetcode will accept

--- leetcode/python/21_LC_Merge_two_sorted_lists.py
+++ leetcode/python/21_LC_Merge_two_sorted_lists.py
@@ -28,13 +28,34 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
     
     return final_list
 
+# alternative solution might be with use of final_list.extend(list1[i:]) final_list.extend(list2[j:])
+
+
 list1 = [1,2,5,6,7,8]
 list2 = [1,3,4]
 print(mergeTwoLists(list1, list2))
 
 
 
-# dlaczego mam odpowiedz [1, 2, 1, 2, 4, 1, 3, 4] ? 
+
+### rozwiazanie dla ListNodes: 
+
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    
+    dummy = ListNode(0)
+    current = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+    
+    current.next = list1 if list1 else list2
+    return dummy.next
 
 
 
