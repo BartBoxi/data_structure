e5cfa65bc4f0e36c3df1c4c5d09bb8ac6befcb48
bartoszpudlo <bartoszpudlo07@gmail.com>
2026-04-29 09:01:08 +0200

    did one more time this task to practice a bit more

--- leetcode/python/logs_to_average.py
+++ leetcode/python/logs_to_average.py
@@ -1,3 +1,64 @@
+# # Given a file consisting of lines like this:
+# # 2017-02-01T20:00 OperationA Start
+# # 2017-02-01T20:01 OperationA End
+# # 2017-02-01T20:08 OperationB Start
+# # 2017-02-01T20:09 OperationC Start
+# # 2017-02-01T20:10 OperationB End
+# # 2017-02-01T20:12 OperationC End
+# # Produce an average runtime of all operations.
+# # Example output:
+# # Average: 0 days 0 hours 2 minutes (0 days 0 hours 6 minutes total for 3 operations) 
+
+
+
+# from datetime import datetime
+# from typing import List 
+
+# def findAverage(logs:List[str]) -> int:
+    
+#     starts = {}
+#     durations = []
+    
+#     for log in logs:
+#         ## you have to split the input from strings 
+        
+#         time_str, op_name, op_type = log.split()
+        
+        
+#         time_converted = datetime.strptime(time_str, '%Y-%m-%dT%H:%M')
+        
+#         if op_type == "Start":
+            
+#             starts[op_name] = time_converted
+            
+#         elif op_type == 'End':
+#             # counting the diff and changing to minutes 
+#             duration = (time_converted - starts[op_name]).total_seconds() / 60
+#             durations.append(duration) 
+#     if durations: 
+#         avg_runtime = sum(durations) / len(durations)
+            
+#     return avg_runtime
+
+
+
+# logs = [
+#     "2017-02-01T20:00 OperationA Start",
+#     "2017-02-01T20:01 OperationA End",
+#     "2017-02-01T20:08 OperationB Start",
+#     "2017-02-01T20:09 OperationC Start",
+#     "2017-02-01T20:10 OperationB End",
+#     "2017-02-01T20:12 OperationC End"
+# ]
+
+
+# print(findAverage(logs))
+
+
+
+
+
+
 # Given a file consisting of lines like this:
 # 2017-02-01T20:00 OperationA Start
 # 2017-02-01T20:01 OperationA End
@@ -11,36 +72,38 @@
 
 
 
-from datetime import datetime 
+from datetime import datetime
+from typing import List 
 
-def findAverage(logs:List[str]) -> int:
+
+def findAverage(file:List[str])-> float:
+    
+    # first i need to break down this input into smaller chunks (string of strings)
     
     starts = {}
     durations = []
+    suma = 0
     
-    for log in logs:
-        ## you have to split the input from strings 
+    
+    for i in file: 
+        op_time, op_name, op_type = i.split()
         
-        time_str, op_name, op_type = log.split()
-        
-        
-        time_converted = datetime.strptime(time_str, '%Y-%m-%dT%H:%M')
-        
-        if op_type == "Start":
-            starts[op_name] = time_converted
-            
-        elif op_type == 'End':
-            # counting the diff and changing to minutes 
-            duration = (time_converted - starts[op_name]).total_seconds() / 60
-            durations.append(duration) 
-    if durations: 
-        avg_runtime = sum(durations) / len(durations)
-            
-    return avg_runtime
+        if op_type == 'Start':  
+            starts[op_name] = op_time ## starts = {OperationA:"2017-02-01T20:00"}
+        else:
+            duration = (datetime.fromisoformat(op_time) - datetime.fromisoformat(starts[op_name])).total_seconds() / 60 # here we also need to convert the time 
+            durations.append(duration)
+    
+    suma = sum(durations) ## this is more pythonic way of doing that 
+    
+    average = suma / len(durations)
+    
+    return average
 
 
 
-logs = [
+
+file = [
     "2017-02-01T20:00 OperationA Start",
     "2017-02-01T20:01 OperationA End",
     "2017-02-01T20:08 OperationB Start",
@@ -49,5 +112,5 @@ logs = [
     "2017-02-01T20:12 OperationC End"
 ]
 
+print(findAverage(file))
 
-print(findAverage(logs))
